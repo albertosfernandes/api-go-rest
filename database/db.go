eed284b8326a7d0587b0a2e5f5ba17791e731b41
albertosouzafernandes <alberto.souzafernandes@gmail.com>
2023-10-21 13:16:05 -0300

    correção importar fmt

--- database/db.go
+++ database/db.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/albertosfernandes/api-go-rest/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -14,7 +17,7 @@ var (
 )
 
 func Dbconnect() {
-	var connectionString = fmt.Spintf("%s:%s@tcp(%s)/%s", os.Getenv("DB_USUARIO"), os.Getenv("DB_SENHA"), os.Getenv("DB_HOST"), os.Getenv("DB_DATABASE"))
+	var connectionString = fmt.Sprintf("%s:%s@tcp(%s)/%s", os.Getenv("DB_USUARIO"), os.Getenv("DB_SENHA"), os.Getenv("DB_HOST"), os.Getenv("DB_DATABASE"))
 	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		panic("Falha na conexão com o banco de dados: " + err.Error())
